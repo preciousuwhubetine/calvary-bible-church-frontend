a02c2129b3f8d3ea79cc4b4f1293d177f9c519f7
Precious Uwhubetine <preciousbetine@gmail.com>
2026-05-18 19:20:17 +0100

    Design leadership page

--- src/application/views/LeadershipPage/LeadershipPage.jsx
+++ src/application/views/LeadershipPage/LeadershipPage.jsx
@@ -7,122 +7,32 @@ function LeadershipPage() {
 
       <ul>
         <li>
-          <img />
+          <img src="/homepage-leadership-overseers-image.jpg" />
 
           <div>
-            <h3>Pastor Name</h3>
+            <h3>Pastor Olumide Emmanuel</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta provident molestiae doloremque quia hic, tenetur quisquam eos nihil temporibus eum vero aliquid, laboriosam illo quaerat inventore error. Esse, unde minus.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta provident molestiae doloremque quia hic, tenetur quisquam eos nihil temporibus eum vero aliquid, laboriosam illo quaerat inventore error. Esse, unde minus.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta provident molestiae doloremque quia hic, tenetur quisquam eos nihil temporibus eum vero aliquid, laboriosam illo quaerat inventore error. Esse, unde minus.
+              Pastor Olumide Emmanuel has been the heart and soul of Calvary Bible Church for over 35 years. As the founding Overseer, he has transformed a small gathering into a vibrant, Bible-centered community where members are inspired to live out their faith as true value creators. He is a prolific author with more than 100 books to his name, and an esteemed speaker in demand globally. His teachings emphasize practical applications of faith, encouraging people to become resilient, purpose-driven leaders who address real-world challenges with integrity and hope. Under his leadership, Calvary Bible Church thrives as a beacon of light in the city. Pastor Emmanuel continues to foster a culture of empowerment and growth, equipping each member to fulfill their God-given purpose and make a meaningful impact in their communities.
             </p>
 
-            <a href="#">
+            {/* <a href="#">
               Learn More
 
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                 <path d="M2.0013 8.00195H13.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M9.33333 3.33464L14 8.0013L9.33333 12.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
-            </a>
+            </a> */}
           </div>
         </li>
 
-        <li>
+        {/* <li>
           <img />
 
           <div>
             <h3>Pastor Name</h3>
-            <a href="#">
-              Read Bio
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M2.0013 8.00195H13.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M9.33333 3.33464L14 8.0013L9.33333 12.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </a>
           </div>
-        </li>
-
-        <li>
-          <img />
-
-          <div>
-            <h3>Pastor Name</h3>
-            <a href="#">
-              Read Bio
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M2.0013 8.00195H13.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M9.33333 3.33464L14 8.0013L9.33333 12.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </a>
-          </div>
-        </li>
-
-        <li>
-          <img />
-
-          <div>
-            <h3>Pastor Name</h3>
-            <a href="#">
-              Read Bio
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M2.0013 8.00195H13.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M9.33333 3.33464L14 8.0013L9.33333 12.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </a>
-          </div>
-        </li>
-
-        <li>
-          <img />
-
-          <div>
-            <h3>Pastor Name</h3>
-            <a href="#">
-              Read Bio
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M2.0013 8.00195H13.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M9.33333 3.33464L14 8.0013L9.33333 12.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </a>
-          </div>
-        </li>
-
-        <li>
-          <img />
-
-          <div>
-            <h3>Pastor Name</h3>
-            <a href="#">
-              Read Bio
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M2.0013 8.00195H13.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M9.33333 3.33464L14 8.0013L9.33333 12.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </a>
-          </div>
-        </li>
-
-        <li>
-          <img />
-
-          <div>
-            <h3>Pastor Name</h3>
-            <a href="#">
-              Read Bio
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M2.0013 8.00195H13.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M9.33333 3.33464L14 8.0013L9.33333 12.668" stroke="black" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </a>
-          </div>
-        </li>
+        </li> */}
       </ul>
     </div>
   )
