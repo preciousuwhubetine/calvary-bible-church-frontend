5d8a1e341891eacf6b211147fea104f5c017299c
Precious Uwhubetine <preciousbetine@gmail.com>
2026-05-20 14:34:30 +0100

    Update 404 page

--- src/application/views/404/404.jsx
+++ src/application/views/404/404.jsx
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom'
 function _404() {
   return (
     <div className={styles['_404']}>
-      <h1>Not Found!</h1>
+      <img src="/icon.png" />
+      <h2>Not Found!</h2>
       <p>The page you are looking for does not exist.</p>
 
       <Link to="/">
